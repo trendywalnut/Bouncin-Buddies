e3065af40b141f777f75d3a79643fb600e743c69
ethan-godwinn <55867179+ethan-godwinn@users.noreply.github.com>
2022-02-27 22:46:16 -0600

    gameplay adjustments and asset implementation

--- game.js
+++ game.js
@@ -32,7 +32,7 @@ var wKey;
 var aKey;
 var dKey;
 
-var playerSpeed = 160;
+var playerSpeed = 220;
 var jumpSpeed = 450;
 
 firstScene.preload = function() {
@@ -40,8 +40,9 @@ firstScene.preload = function() {
     this.load.spritesheet('guy_blue', 'assets/guy_spritesheet_blue.png', { frameWidth: 366, frameHeight: 252});
     this.load.image('balloon', 'assets/balloon.png');
     //this.load.image('guy_red', 'assets/guy_idle_red.png');
-    this.load.image('sky', 'assets/background.png');
-    this.load.image('platform', 'assets/platform.png');
+    this.load.image('sky', 'assets/background_sky2d.png');
+    this.load.image('platform', 'assets/platform_grass.png');
+    this.load.image('ground', 'assets/foreground_grass.png');
 }
 
 firstScene.create = function() {
@@ -49,14 +50,14 @@ firstScene.create = function() {
     
     platforms = this.physics.add.staticGroup();
     
-    platforms.create(400, 568, 'platform').setScale(2).refreshBody();
+    platforms.create(400, 568, 'ground').setScale(1).refreshBody();
     
     platforms.create(600, 400, 'platform');
     platforms.create(50, 250, 'platform');
     platforms.create(750, 220, 'platform');
     
     player_red = this.physics.add.sprite(100, 450, 'guy_red').setScale(0.2);
-    player_red.setBounce(0);
+    player_red.setBounce(0.1);
     player_red.setCollideWorldBounds(true);
     
     this.anims.create({
@@ -83,7 +84,7 @@ firstScene.create = function() {
     
     
     player_blue = this.physics.add.sprite(700, 450, 'guy_blue').setScale(0.2);
-    player_blue.setBounce(0);
+    player_blue.setBounce(0.2);
     player_blue.setCollideWorldBounds(true);
     
     this.anims.create({
@@ -111,16 +112,20 @@ firstScene.create = function() {
     //balloon group
     balloons = this.physics.add.group({
         key: 'balloon',
-        repeat: 11,
+        repeat: 3,
         setXY: {x: 12, y: 0, stepX: 70}
     });
     
+    
     balloons.children.iterate(function(child){
         child.setScale(0.5);
-        child.setVelocity(0, Phaser.Math.Between(-1000, 100));
-        child.allowGravity = false;
+        child.setVelocity(0, Phaser.Math.Between(-100, 100));
+        child.allowGravity = true;
+        child.setBounce(0.9);
+        child.setMass(0.3);
+        
     })
-    
+        
     //text
     scoreText = this.add.text(16,16, 'Score: 0', {fontSize: '32px', fill: '#000'});
     
@@ -133,8 +138,8 @@ firstScene.create = function() {
     this.physics.add.collider(balloons, platforms);
     
     //player colliders with balloons
-    //this.physics.add.collider(player_red, balloons, hitBalloon, null, this);
-    //this.physics.add.collider(player_blue, balloons, hitBalloon, null, this);
+    this.physics.add.collider(player_red, balloons, hitBalloon, null, this);
+    this.physics.add.collider(player_blue, balloons, hitBalloon, null, this);
     
     //spawn balloons
     //game.time.events.repeat(Phaser.Timer.SECOND * 2, 10, spawnBalloon, this);
