8167fafdbfb8d7108732d665b8342154583b7b49
trendywalnut <connorb23@utexas.edu>
2022-03-05 15:49:51 -0600

    updated controls
    added two player movement
    
    WASD for red
    arrow keys for blue
    
    fixed bug with blue jumping

--- game.js
+++ game.js
@@ -28,9 +28,8 @@ var scoreText;
 var timer;
 var total = 0;
 
-var wKey;
-var aKey;
-var dKey;
+var keys;
+var cursors;
 
 var playerSpeed = 220;
 var jumpSpeed = 450;
@@ -60,6 +59,10 @@ firstScene.create = function() {
     player_red.setBounce(0.1);
     player_red.setCollideWorldBounds(true);
     
+    player_blue = this.physics.add.sprite(700, 450, 'guy_blue').setScale(0.2);
+    player_blue.setBounce(0.1);
+    player_blue.setCollideWorldBounds(true);
+    
     this.anims.create({
         key: 'red_idle',
         frames: [ { key: 'guy_red', frame: 0}],
@@ -83,9 +86,7 @@ firstScene.create = function() {
     });
     
     
-    player_blue = this.physics.add.sprite(700, 450, 'guy_blue').setScale(0.2);
-    player_blue.setBounce(0.2);
-    player_blue.setCollideWorldBounds(true);
+    
     
     this.anims.create({
         key: 'blue_idle',
@@ -147,10 +148,9 @@ firstScene.create = function() {
     // Input Events
     cursors = this.input.keyboard.createCursorKeys();
     
+    
     //Second Player Keys
-    this.wKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
-    this.aKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
-    this.dKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+    keys = this.input.keyboard.addKeys('A,W,S,D');
 
 
 }
@@ -162,12 +162,12 @@ firstScene.update = function() {
 
 function playerMovement () {
     // Red Movement
-    if (cursors.left.isDown) {
+    if (keys.A.isDown) {
         player_red.setVelocityX(-playerSpeed);
         player_red.flipX = true;
         player_red.anims.play('red_move', true);
     }
-    else if (cursors.right.isDown) {
+    else if (keys.D.isDown) {
         player_red.setVelocityX(playerSpeed);
         player_red.flipX = false;
         player_red.anims.play('red_move', true);
@@ -176,7 +176,7 @@ function playerMovement () {
         player_red.setVelocityX(0);
         player_red.anims.play('red_idle', true);
     }
-    if (cursors.up.isDown && player_red.body.touching.down) {
+    if (keys.W.isDown && player_red.body.touching.down) {
         player_red.setVelocityY(-jumpSpeed);
         //player_red.anims.play('jump', true);
     }
@@ -185,13 +185,12 @@ function playerMovement () {
     }
     
     //Blue Movement 
-    /*
-    if (dKey.isDown) {
+    if (cursors.right.isDown) {
         player_blue.setVelocityX(playerSpeed);
         player_blue.flipX = false;
         player_blue.anims.play('blue_move')
     }
-    else if (aKey.isDown) {
+    else if (cursors.left.isDown) {
         player_blue.setVelocityX(-playerSpeed);
         player_blue.flipX = true;
         player_blue.anims.play('blue_move', true);
@@ -200,12 +199,12 @@ function playerMovement () {
         player_blue.setVelocityX(0);
         player_blue.anims.play('blue_idle', true);
     }
-    if (wKey.isDown && player_red.body.touching.down) {
+    if (cursors.up.isDown && player_blue.body.touching.down) {
         player_blue.setVelocityY(-jumpSpeed);
     } 
     if (!player_blue.body.touching.down){
         player_blue.anims.play('blue_jump', true);
-    } */
+    } 
 }
 
 function hitBalloon(player, balloon){
